c8bac325abf8766e743565f4492f6841b1f5e93e
dhineshkumar-i2i <79562702+dhineshkumar-i2i@users.noreply.github.com>
2021-03-08 19:01:31 +0530

    Completed Code

--- CalculatorJs.js
+++ CalculatorJs.js
@@ -3,29 +3,33 @@
 const value = document.querySelector(".userinput");
 const output = document.querySelector(".output");
 
-document.querySelector("#one").addEventListener("click", read);
-document.querySelector("#two").addEventListener("click", read);
-document.querySelector("#three").addEventListener("click", read);
-document.querySelector("#four").addEventListener("click", read);
-document.querySelector("#five").addEventListener("click", read);
-document.querySelector("#six").addEventListener("click", read);
-document.querySelector("#seven").addEventListener("click", read);
-document.querySelector("#eight").addEventListener("click", read);
-document.querySelector("#nine").addEventListener("click", read);
-document.querySelector("#zero").addEventListener("click", read);
-document.querySelector("#clear").addEventListener("click", clear);
-document.querySelector("#decimal").addEventListener("click", read);
-document.querySelector("#plus").addEventListener("click", operator);
-document.querySelector("#minus").addEventListener("click", operator);
-document.querySelector("#multiply").addEventListener("click", operator);
-document.querySelector("#divide").addEventListener("click", operator);
-document.querySelector("#negate").addEventListener("click", operator);
-document.querySelector("#modulo").addEventListener("click", operator);
-document.querySelector("#equal").addEventListener("click", calculate);
+(function () {
+  document.querySelector("#one").addEventListener("click", read);
+  document.querySelector("#two").addEventListener("click", read);
+  document.querySelector("#three").addEventListener("click", read);
+  document.querySelector("#four").addEventListener("click", read);
+  document.querySelector("#five").addEventListener("click", read);
+  document.querySelector("#six").addEventListener("click", read);
+  document.querySelector("#seven").addEventListener("click", read);
+  document.querySelector("#eight").addEventListener("click", read);
+  document.querySelector("#nine").addEventListener("click", read);
+  document.querySelector("#zero").addEventListener("click", read);
+  document.querySelector("#clear").addEventListener("click", clear);
+  document.querySelector("#decimal").addEventListener("click", read);
+  document.querySelector("#plus").addEventListener("click", read);
+  document.querySelector("#minus").addEventListener("click", read);
+  document.querySelector("#multiply").addEventListener("click", read);
+  document.querySelector("#divide").addEventListener("click", read);
+  document.querySelector("#negate").addEventListener("click", read);
+  document.querySelector("#modulo").addEventListener("click", read);
+  document.querySelector("#equal").addEventListener("click", calculate);
+})();
 
 function read() {
   if (value.textContent === "0") {
     value.textContent = event.target.textContent;
+  } else if (event.target.textContent == "+/-") {
+    output.textContent *= -1;
   } else {
     value.textContent += event.target.textContent;
   }
@@ -34,14 +38,6 @@ function read() {
 function clear() {
   value.textContent = "0";
   output.textContent = "0";
-}
-
-function operator() {
-  if (event.target.textContent == "+/-") {
-    output.textContent *= -1;
-  } else {
-    value.textContent += event.target.textContent;
-  }
 }
 
 function calculate() {
@@ -94,6 +90,9 @@ function calculate() {
         result %= input_array[i + 1];
     }
   }
+
   console.log(result);
-  output.textContent = result;
+  output.textContent = result.toString().includes(".")
+    ? result.toFixed(2)
+    : result;
 }
